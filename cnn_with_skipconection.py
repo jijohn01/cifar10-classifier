4d76392d8347fbf0fb66a52cca9c69213715097c
jijohn01 <jijohn01@gmail.com>
2018-12-19 23:02:39 +0900

    read dataset

--- cnn_with_skipconection.py
+++ cnn_with_skipconection.py
@@ -1,3 +1,36 @@
-import tensorflow as tf
-import numpy as np
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+import tensorflow as tf
+import os
+import re
+import sys
+import tarfile
+import pickle
+import numpy as np
+import matplotlib.pyplot as mt
+
+data_address = "cifar-10-batches-py"
+
+
+def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
+    with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
+        # note the encoding type is 'latin1'
+        batch = pickle.load(file, encoding='latin1')
+
+    features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    labels = batch['labels']
+
+    return features, labels
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def conv_net():
+    ds,labels = load_cfar10_batch(data_address,1)
+    print(np.shape(ds))
+
+conv_net()
